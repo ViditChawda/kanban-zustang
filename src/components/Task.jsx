501123590db39a3075923283850e4944d34dca50
viditchawda <viditchawda301@gmail.com>
2023-06-24 16:54:42 +0530

    delete button

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,15 @@
 import { useStore } from '../store'
 import './Task.css'
+import { AiOutlineDelete } from 'react-icons/ai'
 
 export default function Task({ title }) {
 
-    const task = useStore((store) => store.tasks.find((task) => task.title === title) )
-    
+    const task = useStore((store) => store.tasks.find((task) => task.title === title))
+    const deleteTask = useStore((store) => store.deleteTask)
     return <div className="task">
         <div>{title}</div>
         <div className='bottomWrapper'>
-            <div></div>
+            <div onClick={() => deleteTask(title)}><AiOutlineDelete className='icon-delete'/></div>
             <div className={task.state}> {task.state}</div>
         </div>
     </div>
