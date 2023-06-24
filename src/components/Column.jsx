4977b6d9510b5feddc4c826068192d12c0779b9f
viditchawda <viditchawda301@gmail.com>
2023-06-24 12:54:18 +0530

    add Operation

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useStore } from '../store';
 import './Column.css'
 import Task from './Task'
@@ -6,14 +7,32 @@ import { shallow } from 'zustand/shallow'
 
 export default function Column({ state }) {
     // const tasks = useStore((store) => store.tasks.filter((task) => task.state === state))
+
+    const [text, setText] = useState('');
+    const [open, setOpen] = useState(false);
     const tasks = useStore(
         (store) => store.tasks.filter((task) => task.state === state)
-    )
+    );
+
+    const addTask = useStore((store) => store.addTask)
 
     return <div className="column">
-        <p>{state}</p>
+        <div className='titleWrapper'>
+            <p>{state}</p>
+            <button onClick={() => setOpen(true)}>Add</button>
+        </div>
         {
-            tasks.map((task) => <Task title={task.title} key={task.title}/>)
+            tasks.map((task) => <Task title={task.title} key={task.title} />)
         }
+        {open && <div className='modal'>
+            <div className='modalContent'>
+                <input onChange={e => setText(e.target.value)} value={text}/>
+                <button onClick={() => {
+                    addTask(text, state)
+                    setText('')
+                    setOpen(false)
+                }}>Submit</button>
+            </div>
+        </div>}
     </div>
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,13 +1,15 @@
+import { useStore } from '../store'
 import './Task.css'
 
-const STATUS = 'DONE'
-
 export default function Task({ title }) {
+
+    const task = useStore((store) => store.tasks.find((task) => task.title === title) )
+    
     return <div className="task">
         <div>{title}</div>
         <div className='bottomWrapper'>
             <div></div>
-            <div className={STATUS}>{STATUS}</div>
+            <div className={task.state}> {task.state}</div>
         </div>
     </div>
 }
